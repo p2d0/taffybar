3eb23bb5ba366ed0ba43c6d4f251874cb27f46ee
Patriot720 <cerkin-3@yandex.ru>
2022-06-10 11:25:51 +0300

    tests update

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,17 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Exception
+import System.Taffybar.Util (runCommand)
 import Test.HUnit
 
-import Network.HTTP.Client
-  ( HttpException,
-    Request (requestHeaders),
-    Response (responseBody, responseStatus),
-    defaultManagerSettings,
-    httpLbs,
-    newManager,
-    parseRequest, parseRequest_
-  )
+-- import Network.HTTP.Client
+--   ( HttpException,
+--     Request (requestHeaders),
+--     Response (responseBody, responseStatus),
+--     defaultManagerSettings,
+--     httpLbs,
+--     newManager,
+--     parseRequest, parseRequest_
+--   )
 
 test1 :: Test
-test1 = TestCase (assertEqual "test" 1 1)
+test1 =
+  TestCase
+    ( do
+        r <- createProcess (proc "curl" ["-s", "https://wttr.in/"])
+        -- runCommand "curl" ["-s", "https://wttr.in/"] >>= either print (assertEqual "pepega" "")
+    )
 
 -- testParsing :: Test
 -- testParsing = TestCase $ do
@@ -20,7 +29,6 @@ test1 = TestCase (assertEqual "test" 1 1)
 
 tests :: Test
 tests = TestList [TestLabel "test1" test1]
-
 
 main :: IO Counts
 main = do
